947e686be77f18c35f6348046d079b69764ea32f
Aman Kumar <kumaraman140795@gmail.com>
2024-03-16 19:50:38 +0530

    Add basic accessebility to Progressbar component

--- src/examples/Progressbar/Progressbar.jsx
+++ src/examples/Progressbar/Progressbar.jsx
@@ -1,3 +1,4 @@
+import { MIN, MAX } from "./constants";
 import { getClampedValue } from "./utils";
 import styles from "./style.module.css";
 
@@ -8,9 +9,16 @@ const ProgressBar = ({
 
     return (
         <div className={styles.container}>
-            <div className={styles.progress} style={{
-                width: `${clampedValue}%`
-            }}>
+            <div
+                className={styles.progress}
+                style={{
+                    width: `${clampedValue}%`
+                }}
+                role="progressbar"
+                aria-valuemin={MIN}
+                aria-valuemax={MAX}
+                aria-valuenow={clampedValue}
+            >
                 {clampedValue}%
             </div>
         </div>
