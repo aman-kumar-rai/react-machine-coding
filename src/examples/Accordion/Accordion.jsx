cb856201b6dd1aa6edd4ccc212b975126c6cec78
Aman Kumar <kumaraman140795@gmail.com>
2024-03-16 16:09:45 +0530

    Add basic implementation for Accordion

--- src/examples/Accordion/Accordion.jsx
+++ src/examples/Accordion/Accordion.jsx
@@ -6,20 +6,35 @@ const Accordion = () => {
     const [sections, setSections] = useState([
         {
             heading: "HTML",
-            content: "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser",
-            id: "HTML"
+            content: "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.",
+            id: "HTML",
+            isOpen: false,
         },
         {
             heading: "CSS",
-            content: "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser",
-            id: "CSS"
+            content: "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML.",
+            id: "CSS",
+            isOpen: false,
         },
         {
             heading: "JS",
-            content: "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser",
-            id: "JS"
+            content: "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
+            id: "JS",
+            isOpen: false,
         },
-    ])
+    ]);
+
+    const handleClickToggle = (event) => {
+        const id = event.target.getAttribute("data-section-id");
+        setSections(prevSections => {
+            return prevSections.map(section => {
+                return section.id === id ? {
+                    ...section,
+                    isOpen: !section.isOpen
+                } : section;
+            })
+        });
+    }
 
     return (
         <div>
@@ -28,15 +43,19 @@ const Accordion = () => {
                     <section className={styles.section} key={section.id}>
                         <header className={styles.header}>
                             <span>{section.heading}</span>
-                            <span className={`${styles.icon} ${styles.icon_rotated}`}></span>
+                            <button data-section-id={section.id} onClick={handleClickToggle}>{section.isOpen ? "Close" : "Open"}</button>
                         </header>
-                        <main>
-                            {section.content}
-                        </main>
+                        {
+                            section.isOpen && (
+                                <main>
+                                    {section.content}
+                                </main>
+                            )
+                        }
                     </section>
                 ))
             }
-        </div>
+        </div >
     )
 }
 
